eb0bfabdd958387187d4901415433205ce0891f0
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-17 11:01:50 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+var positionX = 0;
+var positionY = 0;
+
+function drawCheckeredPattern(row,col) {
+  for (var i = 1; i <= row; i++) {
+    positionX = 0;
+    if (i % 2 === 1) {
+      for (var j = 1; j <= col; j++) {
+        if (j % 2 === 1) {
+          context.fillStyle = 'white';
+        } else if (j % 2 === 0) {
+          context.fillStyle = 'black';
+        } else {
+          alert('An error occurred.')
+        }
+        context.fillRect(positionX, positionY, canvas.width / col, canvas.height / row);
+        positionX += canvas.width / col;
+      }
+    } else if (i % 2 === 0) {
+      for (var j = 1; j <= col; j++) {
+        if (j % 2 === 1) {
+          context.fillStyle = 'black';
+        } else if (j % 2 === 0) {
+          context.fillStyle = 'white';
+        } else {
+          alert('An error occurred.')
+        }
+        context.fillRect(positionX, positionY, canvas.width / col, canvas.height / row);
+        positionX += canvas.width / col;
+      }
+    } else {
+      alert('An error occurred.')
+    }
+    positionY += canvas.height / row;
+  }
+}
+
+drawCheckeredPattern(8,8);
